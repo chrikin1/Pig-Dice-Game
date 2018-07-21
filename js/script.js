189681a24c4f2d6a0fcd040417f02cce5dcca0c2
chrikin1 <chrisianbartoo@gmail.com>
2018-07-21 18:32:06 +0300

    implimented dice roll and fully working

--- js/script.js
+++ js/script.js
@@ -1,127 +1,72 @@
-var first player="";
-var second player="";
+PigDice={
+    roll:function () {
+        return parseInt(Math.random() * (6 - 1) + 1);
+    }
+};
+let player1={
+    name:'',
+    score:0
+},
+    player2={
+        name:'',
+        score:0
+};
 
-var throwdice = function () {
-  return Math.floor(6*Math.random())+1;
-}
+$('#start').on('click',function(){
+    player1.name=$('#player1').val();
+    player2.name=$('#player2').val();
+    $('#player1r .name').html(player1.name);
+    $('#player2r .name').html(player2.name);
+});
 
-function Player(turn) {
-  this.roll = 0;
-  this.tempscore = 0;
-  this.totalscore = 0;
-  this.turn = turn;
-  this.playerName;
-}
+let tmp=0;
+let turn=1;
 
-// checking for 1
-Player.prototype.rollone = function() {
-  if (this.roll === 1) {
-  this.tempscore = 0;
-  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
-  // this.changeturn();
-  } else {
-  this.tempscore += this.roll;
-  }
-}
-
-// hold
-Player.prototype.hold = function () {
-  this.totalscore += this.tempscore;
-  this.tempscore = 0;
-  // this.changeturn();
-  alert(this.playerName + ", your turn is over, pass the mouse!");
-}
-
-// // changing turn
-// Player.prototype.changeturn = function () {
-//   if (this.roll ===1) {
-//     this.turn = false;
-//   } else {
-//     this.turn = true;
-//   }
-// }
-// check for 100
-Player.prototype.winnerCheck = function () {
-  if (this.totalscore >= 100) {
-    alert(this.playerName + " You are the winner!");
-  }
-}
-
-Player.prototype.newGame = function () {
-  //debugger;
-  this.roll = 0;
-  this.tempscore = 0;
-  this.totalscore = 0;
-  this.playerName ="";
-}
-
-var clearValues = function(){
-  $(".player1Name").val("");
-  $(".player2Name").val("");
-}
-
-// User Interface
-$(document).ready(function() {
-
-  $("button#start").click(function(event){
-    player1 = new Player(true);
-    player2 =  new Player(false);
-    $(".player-console").show();
-    $(".start-menu").hide();
-
-    var player1Name = $(".player1Name").val();
-    $("#player1Name").text(player1Name);
-
-    var player2Name = $(".player2Name").val();
-    $("#player2Name").text(player2Name);
-
-    player1.playerName=player1Name;
-    player2.playerName=player2Name;
-
-  });
-  $("button#new-game").click(function(event){
-    $(".player-console").hide();
-    clearValues();
-    player1.newGame();
-    player2.newGame();
-    $("#round-total-1").empty();
-    $("#total-score-1").empty();
-    $("#die-roll-1").empty();
-    $("#round-total-2").empty();
-    $("#total-score-2").empty();
-    $("#die-roll-2").empty();
-
-    $(".start-menu").show();
-  });
-
-  $("button#player1-roll").click(function(event){
-    player1.roll = throwdice();
-    $("#die-roll-1").text(player1.roll);
-    player1.rollone();
-    $("#round-total-1").text(player1.tempscore);
-  });
-
-  $("button#player2-roll").click(function(event){
-    player2.roll = throwdice();
-    $("#die-roll-2").text(player2.roll);
-    player2.rollone();
-    $("#round-total-2").text(player2.tempscore);
-  });
-
-  $("button#player1-hold").click(function(event){
-    player1.hold();
-    $("#total-score-1").text(player1.totalscore);
-    $("#round-total-1").empty();
-    $("#die-roll-1").empty();
-    player1.winnerCheck();
-  });
-
-  $("button#player2-hold").click(function(event){
-    player2.hold();
-    $("#total-score-2").text(player2.totalscore);
-    $("#round-total-2").empty();
-    $("#die-roll-2").empty();
-    player2.winnerCheck();
-  });
+$(document).ready(function(){
+    $('#player1r .name').html();
+    $('#total-score-1').html(0);
+    $('#player2r .name').html();
+    $('#total-score-2').html(0);
 
 });
+
+function myTurn(player,score){
+    if(player===turn){
+        tmp+=score;
+    }else{
+        tmp=score;
+    }
+    if(score===1){
+        if(player===1){
+            $('#turn').html('Player 1 score: '+tmp+'<i> Hand over the controls to the next player</i>');
+            $('#player1-roll').prop('disabled',true);
+            $('#player2-roll').prop('disabled',false);
+        }else if(player===2){
+            $('#turn').html('Player 2 score: '+tmp+'<i> Hand over the controls to the next player</i>');
+            $('#player2-roll').prop('disabled',true);
+            $('#player1-roll').prop('disabled',false);
+        }
+    }else{
+        if(player===1){
+            $('#turn').html('Player 1 score: '+tmp);
+            player1.score+=score;
+            $('#total-score-1').html(player1.score);
+        }else if(player===2){
+            $('#turn').html('Player 2 score: '+tmp);
+            player2.score+=score;
+            $('#total-score-2').html(player2.score);
+        }
+    }
+    turn=player;
+    checkWinner(player);
+}
+
+function checkWinner(player){
+    if(player===1){
+        (player1.score>=100)?alert('Player 1 wins'):false;
+    }else if(player === 2){
+        if(player2.score>=100){
+            alert('Player 2 wins');
+        }
+    }
+}
